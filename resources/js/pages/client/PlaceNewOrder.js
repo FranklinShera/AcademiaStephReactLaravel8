8d77147f913a44e3370f0e0e14b41607aa92df53
FranklinShera <fshera96@gmail.com>
2021-06-22 15:41:32 +0300

    STORAGE ORDER (DRAFT) - MODAL

--- resources/js/pages/client/PlaceNewOrder.js
+++ resources/js/pages/client/PlaceNewOrder.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react'
+import React, {Fragment, useEffect, useRef, useState} from 'react'
 import { useDispatch , useSelector } from 'react-redux'
 import {useHistory} from 'react-router'
 
@@ -19,6 +19,8 @@ import {
     fetchPaperTypes,
     fetchSubjectAreas
 } from "../../actions/OrderActions";
+import {Dialog, Transition} from "@headlessui/react";
+import {ExclamationIcon} from "@heroicons/react/outline";
 
 
 
@@ -30,6 +32,13 @@ const Orders = () => {
     const dispatch = useDispatch()
 
     const[localOrders,setLocalOrders] = useState([])
+
+
+    const [open, setOpen] = useState(false)
+
+    const cancelButtonRef = useRef(null)
+
+
 
     const authClient = useSelector( state => state.authClient)
     const { clientAuth } = authClient;
@@ -291,7 +300,7 @@ const Orders = () => {
                newSave.push(...localOrders)
            }
 
-           newSave.push(Formik.values)
+           newSave.unshift(Formik.values)
 
            localStorage.removeItem(LOCAL_STORAGE_KEY)
 
@@ -380,6 +389,82 @@ const Orders = () => {
                  <div className="place-order-page">
                      <h1 className="text-4xl">Order a Paper</h1>
                      <h4 className="mt-6 text-lg text-primary-4">Paper Details</h4>
+
+                     <Transition.Root show={open} as={Fragment}>
+                         <Dialog
+                             as="div"
+                             static
+                             className="fixed z-10 inset-0 overflow-y-auto"
+                             initialFocus={cancelButtonRef}
+                             open={open}
+                             onClose={setOpen}
+                         >
+                             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                                 <Transition.Child
+                                     as={Fragment}
+                                     enter="ease-out duration-300"
+                                     enterFrom="opacity-0"
+                                     enterTo="opacity-100"
+                                     leave="ease-in duration-200"
+                                     leaveFrom="opacity-100"
+                                     leaveTo="opacity-0"
+                                 >
+                                     <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-50 transition-opacity" />
+                                 </Transition.Child>
+
+                                 {/* This element is to trick the browser into centering the modal contents. */}
+                                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                                    &#8203;
+                                  </span>
+                                 <Transition.Child
+                                     as={Fragment}
+                                     enter="ease-out duration-300"
+                                     enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                     enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                     leave="ease-in duration-200"
+                                     leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                 >
+                                     <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                         <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                             <div className="sm:flex sm:items-start">
+                                                 <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                     <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                                 </div>
+                                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                                     <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                                         Saved Orders
+                                                     </Dialog.Title>
+                                                     <div className="mt-2">
+                                                         {localOrders.map((locOrder,index) => (
+
+                                                            <div className="text-sm text-gray-500 hover:text-gray-900 w-full flex justify-between items-center" key={index}>
+                                                                <span className="cursor-pointer">{index+1+". "}{locOrder.topic}</span>
+                                                                <i className="ti-trash text-red-600 cursor-pointer"></i>
+                                                            </div>
+
+                                                         ))}
+                                                     </div>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+
+                                             <button
+                                                 type="button"
+                                                 className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                                 onClick={() => setOpen(false)}
+                                                 ref={cancelButtonRef}
+                                             >
+                                                 Cancel
+                                             </button>
+                                         </div>
+                                     </div>
+                                 </Transition.Child>
+                             </div>
+                         </Dialog>
+                     </Transition.Root>
+
                      <form action="" className="w-5/6 sm:w-3/4 lg:w-3/5 mt-7 mb-14 lg:mb-28 2xl:w-1/2" onSubmit={Formik.handleSubmit}>
 
                          <div className="local-order flex justify-between items-center">
@@ -391,13 +476,16 @@ const Orders = () => {
                                  Save <i className="ti-save ml-1"></i>
                              </span>
 
-                             <select name="selLocalOrder" id="">
-                                 <option value='' selected disabled>Choose Draft </option>
-                                 {localOrders.map((locOrder,index) =>(
-                                     <option value={index} key={index}>{locOrder.topic}</option>
-                                 ))}
+                             <span
+                                 className="draftBtn"
+                                 onClick={e => {
+                                     e.preventDefault();
+                                     setOpen(!open)
+                                 }}
+                             >
+                                 Pick From Drafts <i className="ti-save ml-1"></i>
+                             </span>
 
-                             </select>
 
                          </div>
 
