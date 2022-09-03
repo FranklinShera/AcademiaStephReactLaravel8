db632369d29eee4bc545a7688b893e691990bffd
Franklin Shera <fshera96@gmail.com>
2022-09-03 22:33:43 +0300

    store swap

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import store from "./store";
+import store from "./app/store/index";
 import { Provider } from "react-redux";
 
 import Main from "./Main";
